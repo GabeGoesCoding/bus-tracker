4cb9b96a138e719e679026736315bd6a7e5cf112
gguio <gdguio@outlook.com>
2023-07-12 22:42:12 -0400

    Moved Data.json

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,41 @@
-import { bus1, bus2, bus3 } from "./data"
+const bus1 = {
+    busId: 19990501,
+    status: "FULL",
+    driver: "Mrs. Puff",
+    students: [
+        { name: "Squidward", isPresent: true },
+        { name: "Spongebob", isPresent: true },
+        { name: "Patrick", isPresent: true },
+        { name: "Eugene", isPresent: true },
+        { name: "Sandy", isPresent: true },
+        { name: "Gary", isPresent: true },
+    ]
+}
 
-console.log(bus1);
+const bus2 = {
+    busId: 19890426,
+    status: "PARTIAL",
+    driver: "Roshi",
+    students: [
+        { name: "Goku", isPresent: false },
+        { name: "Piccolo", isPresent: false },
+        { name: "Krillin", isPresent: true },
+        { name: "Gohan", isPresent: true },
+        { name: "Vegeta", isPresent: true },
+        { name: "Trunks", isPresent: true },
+    ]
+}
+
+const bus3 = {
+    busId: 19770525,
+    status: "EMPTY",
+    driver: "Yoda",
+    students: [
+        { name: "Han Solo", isPresent: false },
+        { name: "Ben", isPresent: false },
+        { name: "Leah", isPresent: false },
+        { name: "Luke", isPresent: false },
+        { name: "Darth Vader", isPresent: false },
+        { name: "Chewbacca", isPresent: false },
+    ]
+}

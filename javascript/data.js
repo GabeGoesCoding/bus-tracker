8bba146ce28433025d323011a135b3e1adc096b2
gguio <gdguio@outlook.com>
2023-07-12 22:19:51 -0400

    Added Data

---
+++ javascript/data.js
@@ -0,0 +1,41 @@
+let bus1 = {
+    busId: 19990501,
+    status: "FULL",
+    driver: "Mrs. Puff",
+    students: [
+        { name: "Squidward", isPresent: true },
+        { name: "Spongebob", isPresent: true },
+        { name: "Patrick", isPresent: true },
+        { name: "Eugene", isPresent: true },
+        { name: "Sandy", isPresent: true },
+        { name: "Gary", isPresent: true },
+    ]
+}
+
+let bus2 = {
+    busId: 19890426,
+    status: "PARTIAL",
+    driver: "Roshi",
+    students: [
+        { name: "Goku", isPresent: false },
+        { name: "Piccolo", isPresent: false },
+        { name: "Krillin", isPresent: true },
+        { name: "Gohan", isPresent: true },
+        { name: "Vegeta", isPresent: true },
+        { name: "Trunks", isPresent: true },
+    ]
+}
+
+let bus3 = {
+    busId: 19770525,
+    status: "EMPTY",
+    driver: "Yoda",
+    students: [
+        { name: "Han Solo", isPresent: false },
+        { name: "Ben", isPresent: false },
+        { name: "Leah", isPresent: false },
+        { name: "Luke", isPresent: false },
+        { name: "Darth Vader", isPresent: false },
+        { name: "Chewbacca", isPresent: false },
+    ]
+}

87eec3126434b0a2c7399bbf5fa9a35b0cce699d
gguio <gdguio@outlook.com>
2023-07-17 23:43:26 -0400

    Create bus functionality added

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,5 @@
 import { busMap } from './data.js';
+import { Bus } from './models/bus.js';
 
 function getBusIds() {
     return Array.from(busMap.keys());
@@ -7,6 +8,13 @@ function getBusIds() {
 function displaySideNavBarWithBusIds() {
     const navBar = document.getElementById('sideNav');
     navBar.innerHTML = "";
+
+    const createBusButtonElem = document.createElement('button');
+    createBusButtonElem.setAttribute('id','create-bus');
+    createBusButtonElem.innerHTML = "Create New Bus";
+    createBusButtonElem.addEventListener('click', renderCreateBusForm);
+    navBar.appendChild(createBusButtonElem)
+
     getBusIds().forEach( busId => {
         const busButton = document.createElement('button');
         busButton.setAttribute('bus-id', busId);
@@ -16,13 +24,22 @@ function displaySideNavBarWithBusIds() {
     });
 }
 
+// TODO: display button to add students
+// TODO: display button to remove students
 function displayStudents(event) {
 
     // get bus from search params
     const bus = grabBusByBusIdFromEvent(event);
+    
     // reset students-list
-    const studentList = document.getElementById('students-list')
-    studentList.innerHTML = "";
+    tearDownStudentsList();
+    const studentList = document.getElementById('students-list');
+
+    // tear down bus form
+    tearDownCreateBusForm();
+
+    // tear down bus capacity
+    tearDownBusCapacity();
 
     // display bus capacity
     displayBusCapacity(event);
@@ -56,12 +73,75 @@ function displayBusCapacity(event) {
     document.getElementById('bus-capacity').innerHTML = bus.capacity.toUpperCase();
 }
 
+function tearDownBusCapacity() {
+    document.getElementById('bus-capacity').innerHTML = "";
+}
+
 function grabBusByBusIdFromEvent(event) {
     // grab busId from search params
     const busId = event.target.getAttribute('bus-id');
 
     // grab bus from busId
     return busMap.get(busId);
+}
+
+// create bus function
+// pass two form fields to create a new bus and new students and add them to the current busmap
+// tear down display students
+// render a form for bus id and driver
+// when form is submitted, open up the bus to add students
+
+function renderCreateBusForm() {
+    tearDownBusCapacity();
+    tearDownStudentsList();
+    
+
+    const formElem = document.getElementById('create-bus-form');
+    let formHTML = `
+    <label for="bus-id">Bus Number:</label>
+    <input type="text" name="bus-id" id="bus-id"><br>
+    <label for="driver-name">Driver Name:</label>
+    <input type="text" name="driver-name" id="driver-name"><br>
+    <button type="submit" id="create-bus-button">Submit</button>
+    `;
+    formElem.innerHTML = formHTML;
+
+    const buttonElem = document.getElementById('create-bus-button');
+    buttonElem.addEventListener('click', createBus);
+}
+
+function tearDownCreateBusForm() {
+    document.getElementById('create-bus-form').innerHTML = "";
+}
+
+function tearDownStudentsList() {
+    document.getElementById('students-list').innerHTML = "";
+}
+
+// TODO: Do not allow users to input busIds that are not 3-digits
+// TODO: Prevent users from creating duplicate busses
+function createBus(event) {
+    // After form is submitted, do not refresh the page.
+    event.preventDefault();
+
+    // Grab the form information from the event    
+    const form = event.target.form;
+
+    // Using the form, grab the inputs with querySelector
+    const busIdInput = form.querySelector('#bus-id');
+    const driverNameInput = form.querySelector('#driver-name');
+
+    // Extract the value from the inputs
+    const busId = busIdInput.value;
+    const driverName = driverNameInput.value;
+
+    // create a new Bus object, and set it on the busMap
+    const bus = new Bus(busId, driverName);
+    busMap.set(busId, bus);
+
+    alert(`Success: Bus #${busId} has been created with ${driverName} driving.`);
+
+    displaySideNavBarWithBusIds();
 }
 
 displaySideNavBarWithBusIds();
